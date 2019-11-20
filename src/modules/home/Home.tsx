ff08e4809bb7b036560d15614b88a00a462f120e
marina.santos <marina.santos@luizalabs.com>
2019-11-20 16:50:03 -0300

    feat: add parser to status name

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -2,22 +2,28 @@ import React from 'react';
 import moment from 'moment';
 
 import List from '../common/List';
+
 import columns from './__mocks__/columns.json';
 import data from './__mocks__/data.json';
+import statusList from './__mocks__/status.json';
 
 interface Item {
   createdAt?: string,
+  status?: Number,
   statusUpdateTime?: string,
-} 
+}
 
 const Home: React.FC = () => {
   const parseData = (data: Array<Object>) => {
     const dateFormat: string = 'DD/MM/YYYY hh:mm:ss';
 
-    return data.map((item: Item) => ({
-      ...item,
-      createdAt: moment(item.createdAt).format(dateFormat),
-      statusUpdateTime: moment(item.statusUpdateTime).format(dateFormat),
+    return data.map((order: Item) => ({
+      ...order,
+      createdAt: moment(order.createdAt).format(dateFormat),
+      statusUpdateTime: moment(order.statusUpdateTime).format(dateFormat),
+      statusName: statusList.find(status => (
+        status.code === order.status ? status.name : ''
+      )),
     }));
   }
 
