2f8ea9642e64057d9b6c464aa28fcfcfdc9a8578
marina.santos <marina.santos@luizalabs.com>
2019-11-20 15:34:45 -0300

    feat: create parse to orders date

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import moment from 'moment';
+
 import List from '../common/List';
 import columns from './__mocks__/columns.json';
 import data from './__mocks__/data.json';
 
+interface Item {
+  createdAt?: string,
+  statusUpdateTime?: string,
+} 
+
 const Home: React.FC = () => {
+  const parseData = (data: Array<Object>) => {
+    const dateFormat: string = 'DD/MM/YYYY hh:mm:ss';
+
+    return data.map((item: Item) => ({
+      ...item,
+      createdAt: moment(item.createdAt).format(dateFormat),
+      statusUpdateTime: moment(item.statusUpdateTime).format(dateFormat),
+    }));
+  }
+
   return (
     <div className="home">
       <List
         columns={columns}
-        dataSource={data}
+        dataSource={parseData(data)}
       />
     </div>
   );
